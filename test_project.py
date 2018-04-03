33adc959e99fc1a5874ea726b634393831fee680
Haocheng Zhao <haochengz@outlook.com>
2018-04-03 21:10:51 +0800

    新增首页插入代办事项的功能测试

--- test_project.py
+++ test_project.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class Functional_Test(unittest.TestCase):
@@ -13,6 +14,22 @@ class Functional_Test(unittest.TestCase):
     def test_open_index_page(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("to-do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('to-do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item',
+                )
+        inputbox.send_keys('Buy peacook feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_tag_name('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacook feathers' for row in rows),
+                )
 
     def tearDown(self):
         self.browser.close()
