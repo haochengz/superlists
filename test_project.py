15324a802dd90c0a89f1bdd9ce711ba35156ceea
Haocheng Zhao <haochengz@outlook.com>
2018-04-04 22:50:54 +0800

    引入插入两项todo item的测试

--- test_project.py
+++ test_project.py
@@ -12,6 +12,18 @@ class Functional_Test(unittest.TestCase):
         self.browser = webdriver.Firefox(options=firefox_options)
         self.browser.implicitly_wait(3)
 
+    def check_for_row_of_table_contains_item(self, row_text):
+        # html文件再如不完整将引发selenium查找节点失败的错误
+        time.sleep(2)
+        table = self.browser.find_element_by_id('id_list_table')
+        self.assertIn(row_text, 
+                [row.text for row in table.find_elements_by_tag_name('tr')])
+
+    def submit_a_item_at_index_page(self, item_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
+
     def test_open_index_page(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("to-do", self.browser.title)
@@ -23,20 +35,12 @@ class Functional_Test(unittest.TestCase):
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item',
                 )
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
+        self.submit_a_item_at_index_page("Buy peacock feathers")
+        self.check_for_row_of_table_contains_item("1: Buy peacock feathers")
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        self.submit_a_item_at_index_page("Use peacock feathers to make a fly")
+        self.check_for_row_of_table_contains_item("1: Buy peacock feathers")
+        self.check_for_row_of_table_contains_item("2: Use peacock feathers to make a fly")
 
     def tearDown(self):
         self.browser.close()
