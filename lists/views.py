5cd0b3901be0c651e5ae367009dd416b37df24f5
haochengz <haochengzhao@outlook.com>
2018-04-13 15:19:06 +0800

    bug fixed

--- lists/views.py
+++ lists/views.py
@@ -17,7 +17,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], saving_list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     items = Item.objects.filter(saving_list=list_)
     return render(request, 'list.html',
@@ -31,7 +31,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], saving_list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists_index.html', {'form': form })
